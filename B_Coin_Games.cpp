48c20d98b059a41cc3ded5673f48239071bbe7b8
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-07-04 23:37:28 +0530

    Adding a question

---
+++ B_Coin_Games.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long
+#define ull unsigned long long
+#define for(s, n) for(int i = s; i < n; i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define rev(s) reverse(s.begin(), s.end())
+#define llvec vector<long long>a()
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define vpp vector<pair<int,int>>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+void solve(){
+    ll n; 
+    cin>>n; 
+    string s;
+    cin>>s;
+    int c=0;
+    n--;
+    while(n>=0){
+        if(s[n]=='U')   c++;
+        n--;
+    }
+    if(c&1){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
