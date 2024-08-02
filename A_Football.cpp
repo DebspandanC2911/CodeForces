a3705b9f245d31ae37257df34557cba20063c785
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-08-02 23:36:30 +0530

    Adding non-Contest(practice) codes on Codeforces

---
+++ A_Football.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long
+#define ull unsigned long long
+#define for(s, n) for(int i = s; i < n; i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define rev(s) reverse(s.begin(), s.end())
+#define llvec vector<long long>a()
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define vpp vector<pair<int,int>>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+void solve(){
+    int n; cin >> n;
+    vector<string> s(n);
+    for(0,n){
+        cin>>s[i];
+    }
+    int a1=0,a2=0;
+    string t1=s[0];
+    string t2="";
+    for(0,n){
+        if(s[i].compare(t1)==0){
+            a1++;
+        }
+        else{
+            a2++;
+            t2=s[i];
+        }
+    }
+    if(a1>a2){
+        cout<<t1<<endl;
+    }
+    else{
+        cout<<t2<<endl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+}
