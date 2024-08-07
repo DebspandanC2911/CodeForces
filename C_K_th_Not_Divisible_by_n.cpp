f5b277488c5203336abf2c8951564a970afa36db
DevCodes <126408440+DebspandanC2911@users.noreply.github.com>
2024-08-08 00:10:03 +0530

    Adding practice questions

---
+++ C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long
+#define lli unsigned long long int
+#define for(s, n) for(int i = s; i < n; i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define rev(s) reverse(s.begin(), s.end())
+#define llvec vector<long long>a()
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define vpp vector<pair<int,int>>
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+void solve(){
+    long long int n , k ;
+    cin >> n >> k;
+
+    long long int div = k / (n-1) ;
+    long long int ex = k % (n-1) ;
+    long long int ans = n * div + ex ;
+    if(ex == 0)
+        ans -- ;
+    cout << ans << endl ;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
